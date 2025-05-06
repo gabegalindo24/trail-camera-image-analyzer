1de6b7bfda249ad2ed6b5c3c1f81d592019ca98a
Gabe Galindo <gabegalindo01@gmail.com>
2025-05-05 21:36:35 -0500

    Initial commit of Java Spring Boot project

---
+++ backend - Trail Camera Image Analyzer/TrailCameraImageAnalyzerApplication/src/main/java/com/galindog/TrailCameraImageAnalyzerApplication/TrailCameraImageAnalyzerApplication.java
@@ -0,0 +1,14 @@
+package com.galindog.TrailCameraImageAnalyzerApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailCameraImageAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailCameraImageAnalyzerApplication.class, args);
+		System.out.println("HELLO SPRING BOOT!");
+	}
+
+}
---
+++ backend - Trail Camera Image Analyzer/TrailCameraImageAnalyzerApplication/src/test/java/com/galindog/TrailCameraImageAnalyzerApplication/TrailCameraImageAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galindog.TrailCameraImageAnalyzerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailCameraImageAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
