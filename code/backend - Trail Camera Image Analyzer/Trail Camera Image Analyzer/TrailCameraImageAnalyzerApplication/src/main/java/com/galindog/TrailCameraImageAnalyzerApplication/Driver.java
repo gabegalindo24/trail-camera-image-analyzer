fed0995c878ff82aff2e0cf47ac5072c8499dfc3
Gabe Galindo <gabegalindo01@gmail.com>
2025-05-14 20:25:57 -0500

    updates to html

--- code/backend - Trail Camera Image Analyzer/Trail Camera Image Analyzer/TrailCameraImageAnalyzerApplication/src/main/java/com/galindog/TrailCameraImageAnalyzerApplication/Driver.java
+++ code/backend - Trail Camera Image Analyzer/Trail Camera Image Analyzer/TrailCameraImageAnalyzerApplication/src/main/java/com/galindog/TrailCameraImageAnalyzerApplication/Driver.java
@@ -2,8 +2,6 @@
 // CYBR 408: Principles of Programming Languages & Automata
 // Final Project - Trail Camera Image Analyzer
 // 05/15/2025
-
-
 package com.galindog.TrailCameraImageAnalyzerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
